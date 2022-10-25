4a968d6b048022ed2eed934fe13d3c0bdb6d9dda
Elisa Gracia Beccar <elisagb@live.com>
2022-10-24 22:19:29 -0300

    cambios

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -5,9 +5,10 @@ function ItemListContainer() {
   return (
     <div className="flex">
       <Tarjeta name="Producto 1" img="/img/el beso.jpg" price="2000" />
-      <Tarjeta name="Producto 2" img="/img/venus.jpg" price="2000">
-        {" "}
-      </Tarjeta>
+      <Tarjeta name="Producto 2" img="/img/venus.jpg" price="2000" />
+      <Tarjeta name="Producto 3" img="/img/starry.jpg" price="3000" />
+      <Tarjeta name="Producto 4" img="/img/monalisa.jpg" price="5000" />
+      <Tarjeta name="Producto 5" img="/img/dali.jpg" price="2800" />
     </div>
   );
 }
